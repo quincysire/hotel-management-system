867094cfc7cb2c5715d7f369f03a11bb6db81b71
quincysire <quincy.sire@student.moringaschool.com>
2024-06-22 18:20:06 +0300

    Removed socketio

--- ext.py
+++ ext.py
@@ -1,7 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
-from flask_socketio import SocketIO
 from flask_jwt_extended import JWTManager
 
 
